549433fb27b64cdda5e5270dd6dd11a9d4b17781
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-08-27 22:11:31 -0700

    Initial Commit

---
+++ tic-tac-toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tic-tac-toe
+//
+//  Created by Suthichai Chukaew on 27/8/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Suthichai Chukaew on 27/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
